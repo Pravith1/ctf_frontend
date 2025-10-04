282de803e4da58cc1be63ff922bf69e61eafa839
Nithees Narendra S <narensakthivel05@gmail.com>
2025-10-05 03:29:08 +0530

    Update App.jsx

--- ctf/src/App.jsx
+++ ctf/src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import Leaderboard from './components/Leaderboard';
+import ArenaLogin from './LoginPage.jsx'
 
 function App() {
   return (
     <div>
-      <Leaderboard />
+      <ArenaLogin />
     </div>
   );
 }
