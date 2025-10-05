7494677c6a417de336df7df65276addd41d400e0
Nithees Narendra S <narensakthivel05@gmail.com>
2025-10-05 11:56:36 +0530

    update

--- ctf/src/Pages/Admin.jsx
+++ ctf/src/Pages/Admin.jsx
@@ -5,15 +5,11 @@ import '../App.css';
 
 function Admin() {
   const [activeMenu, setActiveMenu] = useState('View');
-  
+
   return (
-    <div style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
-      <Header />
-      <div className="app-container" style={{ flex: 1 }}>
-        <Sidebar activeMenu={activeMenu} setActiveMenu={setActiveMenu} />
-        <MainContent activeMenu={activeMenu} />
-      </div>
-      <Footer />
+    <div className="app-container">
+      <Sidebar activeMenu={activeMenu} setActiveMenu={setActiveMenu} />
+      <MainContent activeMenu={activeMenu} />
     </div>
   );
 }
